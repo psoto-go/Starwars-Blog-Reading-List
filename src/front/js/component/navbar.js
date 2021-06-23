abec31c1aad22fb59496caca59d35440073704a2
psoto-go <pabletesglez@gmail.com>
2021-06-23 21:06:17 +0000

    card

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,15 +3,52 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<div className="container">
+				<img
+					src="https://logodownload.org/wp-content/uploads/2015/12/star-wars-logo-1-1.png"
+					style={{ width: "100px" }}
+				/>
+
+				<form className="form-inline ml-5">
+					<div className="btn-group">
+						<button
+							type="button"
+							className="btn btn-primary dropdown-toggle"
+							data-toggle="dropdown"
+							aria-haspopup="true"
+							aria-expanded="false">
+							Favorites
+						</button>
+						<div className="dropdown-menu">
+							<a className="dropdown-item" href="#">
+								Action
+							</a>
+							<a className="dropdown-item" href="#">
+								Another action
+							</a>
+							<a className="dropdown-item" href="#">
+								Something else here
+							</a>
+							<div className="dropdown-divider" />
+							<a className="dropdown-item" href="#">
+								Separated link
+							</a>
+						</div>
+					</div>
+				</form>
 			</div>
 		</nav>
+
+		// <nav className="navbar navbar-light bg-light mb-3">
+		// 	<Link to="/">
+		// 		<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+		// 	</Link>
+		// 	<div className="ml-auto">
+		// 		<Link to="/demo">
+		// 			<button className="btn btn-primary">Check the Context in action</button>
+		// 		</Link>
+		// 	</div>
+		// </nav>
 	);
 };
